6df367c2d575d57a242906de31284ff5e88814ea
ZephireNZ <brynleymcdonald@gmail.com>
2017-06-30 08:08:43 +1200

    Pass args to deploy script gcloud call

--- _deploy.sh
+++ _deploy.sh
@@ -4,4 +4,4 @@ GIT_HASH=$(git rev-parse --short=10 HEAD)
 
 bundler install
 bundle exec jekyll build
-gcloud app deploy --version "$GIT_HASH" --no-promote _app.yaml
+gcloud app deploy --version "$GIT_HASH" --no-promote _app.yaml $@
