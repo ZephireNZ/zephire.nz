634006435c2a723841a8080dd1384e87428a541f
ZephireNZ <brynleymcdonald@gmail.com>
2017-06-29 20:53:52 +1200

    Modifiy file names/ignores so that site files do not leak, and build inside deploy

---
+++ _deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GIT_HASH=$(git rev-parse --short=10 HEAD)
+
+bundler install
+bundle exec jekyll build
+gcloud app deploy --version "$GIT_HASH" --no-promote _app.yaml
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-GIT_HASH=$(git rev-parse --short=10 HEAD)
-
-bundler install
-gcloud app deploy --version "$GIT_HASH" --no-promote app.yaml
